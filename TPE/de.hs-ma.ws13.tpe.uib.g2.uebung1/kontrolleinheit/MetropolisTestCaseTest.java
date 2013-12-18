a42a7434a2d071dfb5778aa12bc3253f2747e80f
Johannes Weber <weber.johanes@googlemail.com>
2013-12-18 23:54:52 +0100

    fertig

---
+++ TPE/de.hs-ma.ws13.tpe.uib.g2.uebung1/kontrolleinheit/MetropolisTestCaseTest.java
@@ -0,0 +1,11 @@
+package kontrolleinheit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Johannes
+ * Date: 29.11.13
+ * Time: 18:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class MetropolisTestCaseTest {
+}
